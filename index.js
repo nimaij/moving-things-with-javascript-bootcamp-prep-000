ff8898061c235ec79e3b1a0614f83d9d4c768e55
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-12 18:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace('px','')
+  var right=parseInt(rightNumbers,10)
+  
+  if (right>0){
+    dodget.style.right=`${right-1}px`
+  }
+}
+
+$('#dodger').addEventListener('keydown',function(e){
+  if(e.which===39){
+    moveDodgerRight();
+  }
+})
